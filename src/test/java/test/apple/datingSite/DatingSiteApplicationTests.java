1d009c689a0e4c799c7d14ba906dc92a8fc133e1
Guneet Singh <guneetnagia24@gmail.com>
2020-05-24 11:47:03 +0530

    seed files

---
+++ src/test/java/test/apple/datingSite/DatingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package test.apple.datingSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
